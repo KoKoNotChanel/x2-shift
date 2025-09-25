c6b6fb92925e454c9c33e1a1c9fe09b9869a3824
Mark <72311074+KoKoNotChanel@users.noreply.github.com>
2025-09-25 13:31:04 +0200

    Update Services.php

--- x2crm/x2engine/protected/modules/services/models/Services.php
+++ x2crm/x2engine/protected/modules/services/models/Services.php
@@ -192,28 +192,56 @@ class Services extends X2Model
 	{
 		$criteria = new CDbCriteria;
 
-		// Filtre sur le champ "account" via jointure manuelle
+		// Jointure TOUJOURS présente pour permettre tri et filtre par "account"
+		$criteria->join =
+			'LEFT JOIN x2_contacts c ON c.nameId = t.contactId ' .
+			'LEFT JOIN x2_accounts a ON a.nameId = c.company ';
+
+		// Filtre sur "account" si actif
 		if (!empty($this->account)) {
-			$criteria->join =
-				'LEFT JOIN x2_contacts c ON c.nameId = t.contactId ' .
-				'LEFT JOIN x2_accounts a ON a.nameId = c.company ';
 			$criteria->compare('a.name', $this->account, true);
 		}
 
-		// Filtrage des statuts masqués par profil utilisateur
+		// Filtrage des statuts comme avant
 		foreach ($this->getFields(true) as $fieldName => $field) {
-			if ($fieldName == 'status') { // si le champ existe
-				$hideStatus = CJSON::decode(Yii::app()->params->profile->hideCasesWithStatus); // statuts à masquer
-				if (!$hideStatus) {
+			if ($fieldName == 'status') {
+				$hideStatus = CJSON::decode(Yii::app()->params->profile->hideCasesWithStatus);
+				if (!$hideStatus)
 					$hideStatus = array();
-				}
 				foreach ($hideStatus as $hide) {
 					$criteria->compare('t.status', '<>' . $hide);
 				}
 			}
 		}
-		$criteria->together = true; // obligatoire pour JOIN custom
-		return $this->searchBase($criteria, $pageSize);
+
+		$criteria->together = true;
+
+		// Mapping du tri pour "account"
+		$sort = new SmartSort(
+			get_class($this),
+			isset($this->uid) ? $this->uid : get_class($this)
+		);
+		$sort->attributes = array_merge(
+			array(
+				'account' => array(
+					'asc' => 'a.name ASC',
+					'desc' => 'a.name DESC',
+				),
+			),
+			$this->getSort()
+		);
+		$sort->defaultOrder = 't.lastUpdated DESC, t.id DESC';
+
+		$dataProvider = new SmartActiveDataProvider(get_class($this), array(
+			'sort' => $sort,
+			'pagination' => array('pageSize' => $pageSize),
+			'criteria' => $criteria,
+			'uid' => $this->uid,
+			'dbPersistentGridSettings' => $this->dbPersistentGridSettings,
+			'disablePersistentGridSettings' => $this->disablePersistentGridSettings,
+		));
+		$sort->applyOrder($criteria);
+		return $dataProvider;
 	}
 
 	public function getLastReply()
